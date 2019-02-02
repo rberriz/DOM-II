9a1a4f5d013efde3f1be6252751894815502b3c8
rberriz <rberriz@icloud.com>
2019-02-02 11:52:40 -0500

    partial

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // Your code goes here
 //small change
+const body = document.querySelector('body');
+
+document.addEventListener('keypress', function (event) {
+    body.classList.toggle('colorWheelToggle');
+});
